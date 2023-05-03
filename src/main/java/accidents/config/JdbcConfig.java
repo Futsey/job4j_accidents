58d7370ae1094998cc8fcaf132f0c4561c4b6d41
Fut <shmopls@gmail.com>
2023-05-03 11:09:54 +0500

    @ModelAttribute. Создание инцидента

--- src/main/java/accidents/config/JdbcConfig.java
+++ src/main/java/accidents/config/JdbcConfig.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:db.properties")
+@PropertySource("classpath:db/db.properties")
 @EnableTransactionManagement
 public class JdbcConfig {
 
