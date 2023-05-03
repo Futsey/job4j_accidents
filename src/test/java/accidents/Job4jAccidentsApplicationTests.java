3326b0f92c2fdf1cfd5dff01bd915ffbaa65a295
Fut <shmopls@gmail.com>
2023-05-03 11:57:31 +0500

    Revert "@ModelAttribute. Создание инцидента"
    This reverts commit df993772765c194b1f05ffd3a6dd4d33d70a6b93.

---
+++ src/test/java/accidents/Job4jAccidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package accidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jAccidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
